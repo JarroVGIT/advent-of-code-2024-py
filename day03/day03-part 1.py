319e46620976eba190b6ea9a5ce49b87cb4919a0
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-03 08:20:33 +0000

    Day 3

--- day03/day03-part 1.py
+++ day03/day03-part 1.py
@@ -4,10 +4,21 @@
 
 # Possible imports required:
 from rich import print
-#import re
+import re
 
-with open("./day03/example.txt") as f:
-#with open("./day03/input.txt") as f:
-    content = f.read().split("\n")
+#with open("./day03/example.txt") as f:
+with open("./day03/input.txt") as f:
+    content = f.read().replace("\n", "")
 
+pattern = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)")
 
+result = 0
+
+for m in re.finditer(pattern, content):
+    match = content[m.span()[0]:m.span()[1]]
+    left, right = match.split(",")
+    left = int(left.replace("mul(", ""))
+    right = int(right.replace(")", ""))
+    result += left * right
+
+print(result)
--- day03/day03-part 2.py
+++ day03/day03-part 2.py
@@ -4,11 +4,30 @@
 
 # Possible imports required:
 from rich import print
-#import re
+import re
 
-with open("./day03/example.txt") as f:
+#with open("./day03/example.txt") as f:
 #with open("./day03/example-2.txt") as f:
-#with open("./day03/input.txt") as f:
-    content = f.read().split("\n")
+with open("./day03/input.txt") as f:
+    content = f.read().replace("\n", "")
+
+pattern = r"(?:mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\))"
+
+do = True
+result = 0
+for m in re.findall(pattern, content):
+    if m == "do()":
+        do = True
+    elif m == "don't()":
+        do = False
+    elif do:
+        left, right = m.split(",")
+        left = int(left.replace("mul(", ""))
+        right = int(right.replace(")", ""))
+        result += left * right
+    else:
+        continue
+
+print(result)
 
 
