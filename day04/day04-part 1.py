01e2d0942495fc874563df7a3b37729e1b590ff4
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-04 08:30:22 +0000

    day 4

--- day04/day04-part 1.py
+++ day04/day04-part 1.py
@@ -6,8 +6,39 @@
 from rich import print
 #import re
 
-with open("./day04/example.txt") as f:
-#with open("./day04/input.txt") as f:
+#with open("./day04/example.txt") as f:
+with open("./day04/input.txt") as f:
     content = f.read().split("\n")
+
+grid = {}
+xs = []
+for y, row in enumerate(content):
+    for x, val in enumerate(row):
+        grid[complex(x,y)] = val
+        if val == "X":
+            xs.append(complex(x,y))
+
+directions = [
+    [complex(1,0), complex(2,0),complex(3,0)],
+    [complex(1,1), complex(2,2),complex(3,3)],
+    [complex(0,1), complex(0,2),complex(0,3)],
+    [complex(-1,1), complex(-2,2),complex(-3,3)],
+    [complex(-1,0), complex(-2,0),complex(-3,0)],
+    [complex(-1,-1), complex(-2,-2),complex(-3,-3)],
+    [complex(0,-1), complex(0,-2),complex(0,-3)],
+    [complex(1,-1), complex(2,-2),complex(3,-3)],
+]
+found = 0
+found_coords = set()
+for xloc in xs:
+    for direction in directions:
+        s = "X"
+        for next_coord in direction:
+            next_val = grid.get(xloc+next_coord, " ")
+            s = s + next_val
+        if s == "XMAS":
+            found += 1
+
+print(found)
 
 
--- day04/day04-part 2.py
+++ day04/day04-part 2.py
@@ -6,9 +6,30 @@
 from rich import print
 #import re
 
-with open("./day04/example.txt") as f:
+#with open("./day04/example.txt") as f:
 #with open("./day04/example-2.txt") as f:
-#with open("./day04/input.txt") as f:
+with open("./day04/input.txt") as f:
     content = f.read().split("\n")
 
+grid = {}
+for y, row in enumerate(content):
+    for x, val in enumerate(row):
+        grid[complex(x,y)] = val
 
+found = 0
+for y in range(len(content)):
+    for x, val in enumerate(content[y]):
+        if val == "A":
+            # A . B
+            # . a . 
+            # C . D
+            a = grid.get(complex(x-1, y-1), " ")
+            b = grid.get(complex(x+1, y-1), " ")
+            c = grid.get(complex(x-1, y+1), " ")
+            d = grid.get(complex(x+1, y+1), " ")
+            if ((a=="M" and d=="S") or (a=="S" and d=="M")) and ((b=="M" and c=="S") or (b=="S" and c=="M")):
+                found += 1
+        else: 
+            continue
+
+print(found)
