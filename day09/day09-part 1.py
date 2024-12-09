0d850d547b5b5e1e0d6ba825403b3916a35f2550
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-09 08:13:43 +0100

    Day 9 part 1

--- day09/day09-part 1.py
+++ day09/day09-part 1.py
@@ -5,9 +5,54 @@
 # Possible imports required:
 from rich import print
 #import re
+import time 
+import copy
 
-with open("./day09/example.txt") as f:
-#with open("./day09/input.txt") as f:
+#with open("./day09/example.txt") as f:
+with open("./day09/input.txt") as f:
     content = f.read().split("\n")
 
+def elapsed(start_time):
+    elapsed = time.time() - start_time
+    minutes = int(elapsed // 60)
+    seconds = int(elapsed % 60)
+    milliseconds = int((elapsed % 1) * 1000)
+    return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 
+start_time = time.time()
+
+disk = []
+total = 0
+for idx, number in enumerate(content[0]):
+    if idx % 2 == 0:
+        file_index = ((idx+2) // 2) - 1
+        disk += [file_index]*int(number)
+    else:
+        disk += [None]*int(number)
+    total += int(number)
+
+back_search_idx = -1
+fwd_search_idx = 0
+
+for idx in range(len(disk)):
+    if (len(disk) + back_search_idx) <= fwd_search_idx:
+        break
+    if disk[back_search_idx] is None: # None in the back
+        back_search_idx -= 1
+        continue
+    if disk[fwd_search_idx] is not None: # Number in the front
+        fwd_search_idx += 1
+    else: # Replace None in the front with val in the back
+        disk[fwd_search_idx] = disk[back_search_idx]
+        disk[back_search_idx] = None
+        fwd_search_idx += 1
+        back_search_idx -= 1
+
+result = 0
+
+for idx, val in enumerate(disk):
+    if val is None:
+        break
+    result += idx*val
+
+print(f"Part 1: {result}, {elapsed(start_time)}")
--- day09/day09-part 2.py
+++ day09/day09-part 2.py
@@ -5,6 +5,7 @@
 # Possible imports required:
 from rich import print
 #import re
+import time
 
 with open("./day09/example.txt") as f:
 #with open("./day09/example-2.txt") as f:
@@ -12,3 +13,12 @@ with open("./day09/example.txt") as f:
     content = f.read().split("\n")
 
 
+
+def elapsed(start_time):
+    elapsed = time.time() - start_time
+    minutes = int(elapsed // 60)
+    seconds = int(elapsed % 60)
+    milliseconds = int((elapsed % 1) * 1000)
+    return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
+
+start_time = time.time()
