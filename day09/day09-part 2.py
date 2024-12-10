69d8c902ad45ad60b3bdd8eae43c8ddf48cf9ae8
Jarro van Ginkel <rob7609@robeco.nl>
2024-12-10 11:18:37 +0100

    Day 9 part 2. List

--- day09/day09-part 2.py
+++ day09/day09-part 2.py
@@ -3,13 +3,14 @@
 # Part 2
 
 # Possible imports required:
-from rich import print
 #import re
 import time
 
-with open("./day09/example.txt") as f:
+from rich import print
+
+#with open("./day09/example.txt") as f:
 #with open("./day09/example-2.txt") as f:
-#with open("./day09/input.txt") as f:
+with open("./day09/input.txt") as f:
     content = f.read().split("\n")
 
 
@@ -22,3 +23,35 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 
 start_time = time.time()
+
+disk = []
+for idx, number in enumerate(content[0]):
+    if idx % 2 == 0:
+        file_index = ((idx+2) // 2) - 1
+        disk.append([file_index]*int(number))
+    else:
+        disk.append([None]*int(number))
+
+for back_idx in range(len(disk)-1, 0, -1):
+    if len(disk[back_idx]) > 0 and disk[back_idx][0] is not None:
+        for fwd_idx in range(0, len(disk), 1):
+            if fwd_idx == back_idx:
+                break
+            if len(disk[fwd_idx]) > 0 and disk[fwd_idx][0] is None:
+                if len(disk[back_idx]) <= len(disk[fwd_idx]):
+                    rest = [None] * (len(disk[fwd_idx])-len(disk[back_idx]))
+                    disk[fwd_idx] = disk[back_idx]
+                    disk[back_idx] = [None] * len(disk[back_idx])
+                    if rest:
+                        disk.insert(fwd_idx + 1, rest)
+                    break
+
+result = 0
+
+disk = [i for part in disk for i in part]
+for idx, val in enumerate(disk):
+    if val is None:
+        continue
+    result += idx*val
+
+print(f"Part 2: {result}, {elapsed(start_time)}")
