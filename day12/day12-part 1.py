0f9a1b3900361165d9497f4dee28abf35eef570c
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-12 07:03:15 +0100

    D12 part 1 correct

--- day12/day12-part 1.py
+++ day12/day12-part 1.py
@@ -3,8 +3,8 @@
 from rich import print
 import time
 
-with open("./day12/example.txt") as f:
-#with open("./day12/input.txt") as f:
+#with open("./day12/example.txt") as f:
+with open("./day12/input.txt") as f:
     content = f.read().split("\n")
 
 def elapsed(start_time):
@@ -15,9 +15,36 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 start_time = time.time()
 
+grid: dict[complex, str] = {}
+
+for y, row in enumerate(content):
+    for x, val in enumerate(row):
+        grid[complex(x,y)] = val
+
+neighbors = [complex(1,0), complex(0,1), complex(-1,0), complex(0, -1)]
+
+seen = set()
+
+def get_perim_area(loc: complex, val: str, seen: set[complex]):
+    area = 1
+    next_neighbors = [loc+n for n in neighbors if grid.get(loc+n)==val and loc+n not in seen]
+    perim = 4-len([loc+n for n in neighbors if grid.get(loc+n)==val])
+    seen.update(next_neighbors)
+    for neigbor in next_neighbors:
+        n_area, n_perim = get_perim_area(neigbor, val,seen)
+        area += n_area
+        perim += n_perim
+    return area, perim
+
+all_perim_area = []
+for loc, val in grid.items():
+    if loc in seen:
+        continue
+    seen.add(loc)
+    total_perimeter, area = get_perim_area(loc, val, seen)
+    all_perim_area.append((total_perimeter, area))
 
 
-
-result = 0
+result = sum([comb[0]*comb[1] for comb in all_perim_area])
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
