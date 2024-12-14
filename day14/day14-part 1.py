c3e4a5839ddc85b4aee13f085f00f309662cb367
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-14 20:38:30 +0100

    D14, tough.

--- day14/day14-part 1.py
+++ day14/day14-part 1.py
@@ -2,10 +2,14 @@
 # Author: Jarro van Ginkel
 from rich import print
 import time
+from collections import defaultdict
 
-with open("./day14/example.txt") as f:
-#with open("./day14/input.txt") as f:
+#with open("./day14/example.txt") as f:
+with open("./day14/input.txt") as f:
     content = f.read().split("\n")
+
+len_x = 101
+len_y = 103
 
 def elapsed(start_time):
     elapsed = time.time() - start_time
@@ -15,9 +19,31 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 start_time = time.time()
 
+robots = {}
+for idx, line in enumerate(content):
+    p, v = line.split(" ")
+    x, y = p.replace("p=", "").split(",")
+    dx, dy = v.replace("v=", "").split(",")
+    robots[idx] = {"x": int(x), "y": int(y), "dx": int(dx), "dy": int(dy)}
 
+locs = defaultdict(lambda:0)
+for robot, f in robots.items():
+    new_x = (f["x"] + 100 * f["dx"]) % len_x
+    new_y = (f["y"] + 100 * f["dy"]) % len_y
+    locs[(new_x, new_y)] += 1
 
+q1, q2, q3, q4 = 0, 0, 0, 0
 
-result = 0
+for loc, count in locs.items():
+    if loc[0] < len_x//2 and loc[1] < len_y//2:
+        q1 += count
+    elif loc[0] < len_x//2 and loc[1] > len_y//2:
+        q2 += count
+    elif loc[0] > len_x//2 and loc[1] < len_y//2:
+        q3 += count
+    elif loc[0] > len_x//2 and loc[1] > len_y//2:
+        q4 += count
+print(q1, q2, q3, q4)
+result = q1 * q2 * q3 * q4
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day14/day14-part 2.py
+++ day14/day14-part 2.py
@@ -2,10 +2,14 @@
 # Author: Jarro van Ginkel
 from rich import print
 import time
+from collections import defaultdict
 
-with open("./day14/example.txt") as f:
-#with open("./day14/input.txt") as f:
+#with open("./day14/example.txt") as f:
+with open("./day14/input.txt") as f:
     content = f.read().split("\n")
+
+len_x = 101
+len_y = 103
 
 def elapsed(start_time):
     elapsed = time.time() - start_time
@@ -15,9 +19,50 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 start_time = time.time()
 
+robots = {}
+for idx, line in enumerate(content):
+    p, v = line.split(" ")
+    x, y = p.replace("p=", "").split(",")
+    dx, dy = v.replace("v=", "").split(",")
+    robots[idx] = {"x": int(x), "y": int(y), "dx": int(dx), "dy": int(dy)}
 
+# Going to search for 5x5 squares that are completely filled for each second-state.
+squares = [
+    complex(x, y) for x in range(5) for y in range(5)
+]
+
+# Super inefficient but I am tired.
+def check_for_square(locs:set[complex]):
+    for y in range(len_y-4):
+        for x in range(len_x-4):
+            to_check = set([complex(x,y) + s for s in squares])
+            if locs.issuperset(to_check):
+                return True
+    return False
+            
+
+for i in range(1, 10000):
+    locs = set()
+
+    for robot, f in robots.items():
+        new_x = (f["x"] + i * f["dx"]) % len_x
+        new_y = (f["y"] + i * f["dy"]) % len_y
+        locs.add(complex(new_x, new_y))
+    if i % 50 == 0:
+        print(i)
+    if check_for_square(locs):
+        with open(f"./day14/prints/{i}.txt", "w") as f:
+            for y in range(len_y):
+                row = ""
+                for x in range(len_x):
+                    if complex(x,y) in locs:
+                        row += "X"
+                    else:
+                        row += " "
+                f.write(row+"\n")
 
 
 result = 0
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
+print("Check folder 'prints'...")
