768f0ca29f45456ae0d721a7935a88e8d36187b4
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-15 16:29:33 +0100

    Day 15 part 1, idea for part 2 (for after vacation)

--- day15/day15-part 1.py
+++ day15/day15-part 1.py
@@ -3,10 +3,6 @@
 from rich import print
 import time
 
-with open("./day15/example.txt") as f:
-#with open("./day15/input.txt") as f:
-    content = f.read().split("\n")
-
 def elapsed(start_time):
     elapsed = time.time() - start_time
     minutes = int(elapsed // 60)
@@ -15,9 +11,62 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 start_time = time.time()
 
+#with open("./day15/example.txt") as f:
+with open("./day15/input.txt") as f:
+    field, moves = f.read().split("\n\n")
 
+grid = {}
+max_y = len(field.split("\n"))
+max_x = len(field.split("\n")[0])
 
+for y, row in enumerate(field.split("\n")):
+    for x, val in enumerate(row):
+        grid[complex(x,y)] = val
+        if val == "@":
+            current_loc = complex(x,y)
 
+directions = {
+    "v": complex(0, 1),
+    "<": complex(-1, 0),
+    "^": complex(0, -1),
+    ">": complex(1, 0)
+}
+
+def get_next_val(dir: str, current_loc: complex) -> tuple[complex, str]:
+    n = current_loc+directions[dir]
+    return (n, grid.get(n))
+
+for move in moves.replace("\n", ""):
+    neighbor, next_val = get_next_val(move, current_loc)
+    if next_val == "#":
+        continue
+    elif next_val == ".":
+        grid[current_loc] = "."
+        grid[neighbor] = "@"
+        current_loc = neighbor
+    elif next_val == "O":
+        next_loc = neighbor
+        found_stone = False
+        while True:
+            neighbor, next_val = get_next_val(move, neighbor)
+            if next_val == "O":
+                continue
+            elif next_val == ".":
+                break
+            else: # found rock
+                found_stone = True
+                break
+        if not found_stone:
+            # only move entire row of boxes if the last found was not a rock.
+            grid[neighbor] = "O"
+            grid[current_loc] = "."
+            current_loc = next_loc
+            
 result = 0
+
+for y in range(max_y):
+    for x in range(max_x):
+        if grid[complex(x,y)] == "O":
+            result += x + 100*y
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day15/day15-part 2.py
+++ day15/day15-part 2.py
@@ -5,7 +5,7 @@ import time
 
 with open("./day15/example.txt") as f:
 #with open("./day15/input.txt") as f:
-    content = f.read().split("\n")
+    field, moves = f.read().split("\n\n")
 
 def elapsed(start_time):
     elapsed = time.time() - start_time
@@ -15,7 +15,15 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 start_time = time.time()
 
-
+# idea: creation of grid is doable, still create grid in 
+# dict, so with complex(x,y) = "[" etc. When moving, up or down
+# think in list of ranges. If the direct above is [, then look up on 
+# the range with the accompanying ]. If overlap is half (e.g ] of a box is on 
+# top of [ of the box directly above, redo the while loop but with a list of
+# ranges to keep track what to check next. If all ranges have a . above then, then
+# move everything up 1. If one has a #, then no move. left/right has no impact. 
+# list of ranges required in case you run into V form of boxes; a # in the middle 
+# should not halt the entire box structure.
 
 
 result = 0
