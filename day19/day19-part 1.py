3fc3c5749bfe6eec6052dbb7d08a66fa5dd96b80
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-19 08:17:40 +0100

    Day 19

--- day19/day19-part 1.py
+++ day19/day19-part 1.py
@@ -2,10 +2,12 @@
 # Author: Jarro van Ginkel
 from rich import print
 import time
+from functools import lru_cache
 
-with open("./day19/example.txt") as f:
-#with open("./day19/input.txt") as f:
-    content = f.read().split("\n")
+#with open("./day19/example.txt") as f:
+with open("./day19/input.txt") as f:
+    towels, designs = f.read().split("\n\n")
+
 
 def elapsed(start_time):
     elapsed = time.time() - start_time
@@ -15,9 +17,23 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 start_time = time.time()
 
+towels = towels.split(", ")
+designs = designs.split("\n")
 
-
+@lru_cache
+def deconstruct(remaining_design:str) -> bool:
+    if not remaining_design:
+        return True
+    for t in towels:
+        if remaining_design.startswith(t):
+            if deconstruct(remaining_design[len(t):]):
+                return True
+    return False 
 
 result = 0
+
+for design in designs:
+    if deconstruct(design):
+        result += 1
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day19/day19-part 2.py
+++ day19/day19-part 2.py
@@ -2,10 +2,12 @@
 # Author: Jarro van Ginkel
 from rich import print
 import time
+from functools import lru_cache
 
-with open("./day19/example.txt") as f:
-#with open("./day19/input.txt") as f:
-    content = f.read().split("\n")
+#with open("./day19/example.txt") as f:
+with open("./day19/input.txt") as f:
+    towels, designs = f.read().split("\n\n")
+
 
 def elapsed(start_time):
     elapsed = time.time() - start_time
@@ -15,9 +17,22 @@ def elapsed(start_time):
     return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 start_time = time.time()
 
+towels = towels.split(", ")
+designs = designs.split("\n")
 
-
+@lru_cache
+def deconstruct(remaining_design:str) -> int:
+    if not remaining_design:
+        return 1
+    total_ways = 0
+    for t in towels:
+        if remaining_design.startswith(t):
+            total_ways += deconstruct(remaining_design[len(t):])
+    return total_ways 
 
 result = 0
+
+for design in designs:
+    result +=deconstruct(design)
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
