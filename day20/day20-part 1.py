59739a8624cb1b4a50097b1cb35a3ea06324caa7
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-20 09:53:38 +0100

    Day 20, p2 inefficient

--- day20/day20-part 1.py
+++ day20/day20-part 1.py
@@ -1,11 +1,51 @@
 # Advent of Code 2024 - Day 20 - part 1
 # Author: Jarro van Ginkel
-from aoc_utils import elapsed, parse_data_as_lines, start_time
+from aoc_utils import elapsed, parse_data_as_grid, start_time, print_grid
 from rich import print
 
-content = parse_data_as_lines(20)
+grid = parse_data_as_grid(20)
 
+start = min(k for k, v in grid.items() if v == "S")
+end = min(k for k, v in grid.items() if v == "E")
+grid[start] = "."
+grid[end] = "."
 
-result = 0
+neighbors = [complex(0,-1), complex(1, 0), complex(0,1), complex(-1,0)]
+
+track = [start]
+current_loc = start
+while current_loc != end:
+    for n in neighbors:
+        if current_loc+n not in track and grid.get(current_loc+n) == ".":
+            current_loc = current_loc+n
+            track.append(current_loc)
+
+cheats: list[tuple[complex, complex, int]] = []
+for loc, val in grid.items():
+    if val == ".":
+        continue
+    track_ns = [loc+n for n in neighbors if grid.get(loc+n)=="."]
+    if len(track_ns) >= 2:
+        # sort the locations by occurence in track:
+        track_ns.sort(key=lambda c: track.index(c))
+
+        # there can be multiple cheats possible, e.g.
+        # ...
+        # .#.
+        # .#.
+        # you can go from 0,1 to 1,0, or from 0,1 to 2,1 or from 1,0 to 2,1.   
+        # update; this doesn't seem to matter. Just take first and last in list.
+        c_start = track_ns[0]
+        c_end = track_ns[-1]
+        c_len = c_len = track.index(c_end) - track.index(c_start) - 2
+        cheats.append((c_start, c_end, c_len))
+        # for i, c_start in enumerate(track_ns[:-1]):
+        #     for c_end in track_ns[i+1:]:
+        #         c_len = track.index(c_end) - track.index(c_start) + 2
+        #         cheats.append((c_start, c_end, c_len))
+c = [(s, e, l) for s,e,l in cheats if l >= 100]
+c.sort(key=lambda c: c[2])
+
+result = len(c)
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day20/day20-part 2.py
+++ day20/day20-part 2.py
@@ -1,11 +1,50 @@
 # Advent of Code 2024 - Day 20 - part 2
 # Author: Jarro van Ginkel
-from aoc_utils import elapsed, parse_data_as_lines, start_time
+from aoc_utils import elapsed, parse_data_as_grid, start_time, print_grid
 from rich import print
+from itertools import groupby
+grid = parse_data_as_grid(20)
 
-content = parse_data_as_lines(20)
+start = min(k for k, v in grid.items() if v == "S")
+end = min(k for k, v in grid.items() if v == "E")
+grid[start] = "."
+grid[end] = "."
 
+neighbors = [complex(0,-1), complex(1, 0), complex(0,1), complex(-1,0)]
 
-result = 0
+track = [start]
+current_loc = start
+while current_loc != end:
+    for n in neighbors:
+        if current_loc+n not in track and grid.get(current_loc+n) == ".":
+            current_loc = current_loc+n
+            track.append(current_loc)
+
+# start, end, cheat-gain
+cheats: list[tuple[complex, complex, int]] = []
+
+# cheats of same lenght to same locs (but different paths) are considered the same cheat.
+# for each element in track, get all places in < 20 manhattan distance, 
+# if target tile is on track and more than 100 away on track, add to cheats.
+
+for track_tile in track:
+    seen = []
+    for y in range(-20, 21, 1):
+        allowable_x = 20-abs(y)
+        for x in range(-allowable_x, allowable_x+1, 1):
+            target = track_tile + complex(x,y)
+            if target not in track or target in seen:
+                continue
+            seen.append(target)
+            c_gain = track.index(target) - track.index(track_tile) - abs(x) - abs(y)
+            if c_gain >= 100:
+                cheats.append((track_tile, target, c_gain))
+
+# cheats.sort(key=lambda c: c[2])                
+# c = groupby(cheats, key=lambda c: c[2])
+# for key, group in c:
+#     print(key, len(list(group)))
+# print(cheats[-1])
+result = len(cheats)
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
