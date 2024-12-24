d3d1f3b9008a4131214912236fcf4e496b0b2798
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-24 10:25:41 +0100

    day 23

--- day23/day23-part 1.py
+++ day23/day23-part 1.py
@@ -2,10 +2,28 @@
 # Author: Jarro van Ginkel
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
+from collections import defaultdict
 
 content = parse_data_as_lines(23)
 
+connections: dict[str, set[str]] = defaultdict(set)
 
-result = 0
+for connection in content:
+    lhs, rhs = connection.split("-")
+    connections[lhs].add(rhs)
+    connections[rhs].add(lhs)
+
+three_conns: set[frozenset[str]] = set()
+
+for computer, conns in connections.items():
+    for conn in conns:
+        intersect = list(conns.intersection(connections[conn]))
+        if len(intersect) >= 1:
+            for ins in intersect:
+                if computer.startswith("t") or conn.startswith("t") or ins[0].startswith("t"):
+                    three_conns.add(frozenset([computer, conn, ins]))
+
+
+result = len(three_conns)
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day23/day23-part 2.py
+++ day23/day23-part 2.py
@@ -2,10 +2,17 @@
 # Author: Jarro van Ginkel
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
+import networkx as nx
 
 content = parse_data_as_lines(23)
-
-
 result = 0
+G = nx.Graph()
+for connection in content:
+    lhs, rhs = connection.split("-")
+    G.add_edge(lhs, rhs)
 
+cliques = list(nx.enumerate_all_cliques(G))[-1]
+cliques.sort()
+
+result = ",".join(cliques)
 print(f"Part 2: {result}, {elapsed(start_time)}")
